40a990adfa7e5b1bb2b642d0b4ee258b901c53a9
Kenneth Koch <kennethkoch@Kenneths-MacBook-Pro.local>
2018-03-22 20:29:37 -0400

    started front end, simple Facebook Login

---
+++ PartyAppAndroid/app/src/main/java/edu/duke/compsci290/partyappandroid/EventPackage/AllUserInfo.java
@@ -0,0 +1,14 @@
+package edu.duke.compsci290.partyappandroid.EventPackage;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kennethkoch on 3/22/18.
+ */
+
+public class AllUserInfo {
+    private User mCurrentUser;
+    private ArrayList<Party> mPartiesHosting;
+    private ArrayList<Party> mPartiesBouncing;
+    private ArrayList<Party> mPartiesInvited;
+}
---
+++ PartyAppAndroid/app/src/main/java/edu/duke/compsci290/partyappandroid/EventPackage/User.java
@@ -0,0 +1,17 @@
+package edu.duke.compsci290.partyappandroid.EventPackage;
+
+/**
+ * Created by kennethkoch on 3/22/18.
+ */
+
+public class User {
+    private String mUserName;
+    private String mName;
+    private String mEmail;
+
+    public User(String userName, String name, String email){
+        mUserName = userName;
+        mName = name;
+        mEmail = email;
+    }
+}
