9c6d3e1007dbf3b6a69e14440df40c58528494a0
Derrick <stjohndlee@gmail.com>
2018-03-30 16:20:24 -0400

    wip on api

--- drf/project/api/views.py
+++ drf/project/api/views.py
@@ -4,3 +4,18 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from project.api.serializers import UserSerializer, GroupSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint to allow users to be viewed or edited
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
