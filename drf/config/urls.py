73c3384eef3cb8237ba2cd62355a2fa21f027bd5
Derrick <stjohndlee@gmail.com>
2018-04-02 16:51:45 -0400

    integrate django-rest-framework-social-oauth2

--- drf/config/urls.py
+++ drf/config/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('project.api.urls')),
+    url(r'^auth/', include('rest_framework_social_oauth2.urls')),
 ]
