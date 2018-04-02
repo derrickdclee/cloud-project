2680ef0375899c75793a6f806e549b8e15f03343
Derrick <stjohndlee@gmail.com>
2018-04-02 17:33:12 -0400

    adding User stuff

--- drf/project/api/serializers.py
+++ drf/project/api/serializers.py
@@ -5,13 +5,17 @@ from rest_framework import serializers
 class PartySerializer(serializers.ModelSerializer):
     class Meta:
         model = Party
+        host = serializers.ReadOnlyField(source='host.username')
         fields = ('name', 'description', 'location', 'start_time',
                   'end_time', 'deleted')
 
-# class UserSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ('url', 'username', 'email', 'groups')
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    parties = serializers.PrimaryKeyRelatedField(many=True, queryset=Party.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'parties')
 #
 # class GroupSerializer(serializers.HyperlinkedModelSerializer):
 #     class Meta:
