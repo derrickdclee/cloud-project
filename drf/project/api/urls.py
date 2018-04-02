41c4e23e3bfef9e738dfe3850070e7df73e196d7
Derrick <stjohndlee@gmail.com>
2018-04-01 21:28:35 -0400

    wip on invitation

--- drf/project/api/urls.py
+++ drf/project/api/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from project.api import views
 
 urlpatterns = [
-    url(r'^parties/$', views.party_list),
-    url(r'^parties/(?P<pk>[0-9]+)/$', views.party_detail),
+    url(r'^parties/$', views.PartyList.as_view()),
+    url(r'^parties/(?P<pk>[0-9]+)/$', views.PartyDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
