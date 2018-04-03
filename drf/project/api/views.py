05aa3cb49ba1e17cd1b596ee9e145437682e4fb7
Derrick <stjohndlee@gmail.com>
2018-04-03 16:17:21 -0400

    basic endpoints up and running for Invitation

--- drf/project/api/views.py
+++ drf/project/api/views.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from rest_framework import generics
-from rest_framework import permissions
+from rest_framework import generics, permissions, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-from project.api.models import Party
+from project.api.models import Party, Invitation
 from django.contrib.auth.models import User
-from project.api.serializers import PartySerializer, UserSerializer
+from project.api.serializers import PartySerializer, \
+    UserSerializer, InvitationSerializer
 from project.api.permissions import IsSameUser
 
 
@@ -35,3 +37,17 @@ class UserDetail(generics.RetrieveAPIView):
     serializer_class = UserSerializer
     permission_classes = (IsSameUser,)
 
+
+class InvitationList(generics.ListCreateAPIView):
+    queryset = Invitation.objects.all()
+    serializer_class = InvitationSerializer
+
+    def perform_create(self, serializer):
+        invitee = User.objects.get(pk=self.request.data['invitee_id'])
+        party = Party.objects.get(pk=self.request.data['party_id'])
+        serializer.save(invitee=invitee, party=party)
+
+
+class InvitationDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Invitation.objects.all()
+    serializer_class = InvitationSerializer
