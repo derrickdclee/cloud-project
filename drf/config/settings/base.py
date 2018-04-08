8df2350346c2c835ff90e6b9a21318af91cc3986
Derrick Lee <derrickdclee@gmail.com>
2018-04-07 22:56:27 -0400

    fix static files

--- drf/config/settings/base.py
+++ drf/config/settings/base.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = join(ROOT_DIR, "www", "static")
+STATIC_ROOT = join(ROOT_DIR, "staticfiles")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [join(APPS_DIR, 'static'), ]
 STATICFILES_FINDERS = (
