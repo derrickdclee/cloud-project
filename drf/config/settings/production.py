1a58012fdbdf0ad980d1d08af3ab15f214e306bf
Derrick Lee <derrickdclee@gmail.com>
2018-04-07 22:10:18 -0400

    update config

--- drf/config/settings/production.py
+++ drf/config/settings/production.py
@@ -2,6 +2,7 @@ from .base import *
 
 DEBUG = False
 
+ALLOWED_HOSTS = [".elasticbeanstalk.com"]
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 SOCIAL_AUTH_FACEBOOK_KEY = os.environ.get('SOCIAL_AUTH_FACEBOOK_KEY')
