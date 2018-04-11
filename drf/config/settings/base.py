bffa0386802dc9d7d0f546133ffc10bcf0be3b9b
Derrick Lee <derrickdclee@gmail.com>
2018-04-11 15:28:21 -0400

    fix bug in default logout

--- drf/config/settings/base.py
+++ drf/config/settings/base.py
@@ -152,7 +152,6 @@ MEDIA_URL = '/media/'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
         'rest_framework_social_oauth2.authentication.SocialAuthentication',
