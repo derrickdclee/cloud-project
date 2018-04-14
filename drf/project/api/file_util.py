61dca9cce4d915e4f537a02d70932fa44c2d96b2
Derrick Lee <derrickdclee@gmail.com>
2018-04-13 22:50:34 -0400

    fix a bug in filename

--- drf/project/api/file_util.py
+++ drf/project/api/file_util.py
@@ -3,14 +3,14 @@ import os
 
 
 def set_filename_format(now, instance, filename):
-    return "{party_id}-{date}-{microsecond}{extension}".\
-        format(party_id=instance.id, date=str(now.date()),
+    return "{host_id}-{date}-{microsecond}{extension}".\
+        format(host_id=instance.host.id, date=str(now.date()),
                microsecond=now.microsecond, extension=os.path.splitext(filename)[1])
 
 
 def party_directory_path(instance, filename):
     now = datetime.datetime.now()
-    path = "images/{year}/{month}/{day}/{party_id}/{filename}".\
+    path = "images/{year}/{month}/{day}/{host_id}/{filename}".\
         format(year=now.year, month=now.month, day=now.day,
-               party_id=instance.id, filename=set_filename_format(now, instance, filename))
+               host_id=instance.host.id, filename=set_filename_format(now, instance, filename))
     return path
