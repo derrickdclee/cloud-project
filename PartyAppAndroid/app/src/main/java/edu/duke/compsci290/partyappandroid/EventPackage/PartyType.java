55d9ad05754916675f48c0e7130cfae7352e1200
Derrick Lee <derrickdclee@gmail.com>
2018-04-15 19:17:28 -0400

    Remove old Android stuff for safe merging

--- PartyAppAndroid/app/src/main/java/edu/duke/compsci290/partyappandroid/EventPackage/PartyType.java
+++
@@ -1,9 +0,0 @@
-package edu.duke.compsci290.partyappandroid.EventPackage;
-
-/**
- * Created by kennethkoch on 3/22/18.
- */
-
-public enum PartyType {
-    OPEN, STRICT, MODERATE
-}
--- PartyAppAndroid/app/src/main/java/edu/duke/compsci290/partyappandroid/EventPackage/PartyUserStatus.java
+++
@@ -1,9 +0,0 @@
-package edu.duke.compsci290.partyappandroid.EventPackage;
-
-/**
- * Created by kennethkoch on 3/22/18.
- */
-
-public enum PartyUserStatus {
-    UNINVITED, INVITED, RSVP, CHECKEDIN
-}
