e4e92d759855657dfe7d994a4757d67b0a34e671
Derrick Lee <derrickdclee@gmail.com>
2018-04-15 20:06:24 -0400

    remove django stuff for easy merging

--- drf/config/settings/local.py
+++
@@ -1,5 +0,0 @@
-from .base import *
-
-SECRET_KEY = env('DJANGO_SECRET_KEY', default='54+kk=eo895m371t8myy$zbp(3c!hy5swr%0gmf20ud_2i3d$v')
-
-DEBUG = env.bool('DJANGO_DEBUG', default=True)
--- drf/project/api/admin.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.contrib import admin
-from project.api.models import Party, Invitation
-
-# Register your models here.
-admin.site.register(Party)
-admin.site.register(Invitation)
